f1cd870632bb58cf73df34962a6b29e89c9a33ef
krishnakumar-kapil <kapilk@cs.utexas.edu>
2015-01-29 21:04:42 -0600

    add files

---
+++ Website/js/custom.js
@@ -0,0 +1,18 @@
+$(document).ready( function() {
+
+	// Smooth Wheel
+	$("body").smoothWheel();
+
+
+
+	// ScrollIt configuration
+	$(function () {
+		$.scrollIt({
+			// The easing function for animation
+			easing: 'linear',
+			// How long (in ms) the animation takes
+			scrollTime: 500
+		});
+	});
+
+});
